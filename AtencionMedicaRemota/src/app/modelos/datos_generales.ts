6adf095d06cffe6737fd5518ed680607766c0948
rrichards21 <rodrigo.a.richards@gmail.com>
2020-08-03 19:56:30 -0400

    notificaciones 90% ok

--- AtencionMedicaRemota/src/app/modelos/datos_generales.ts
+++
@@ -1,8 +0,0 @@
-import { Title } from '@angular/platform-browser';
-
-export interface Generales{
-    id?:string,
-    estatura?:string,
-    peso?:string,
-    g_sanguineo?:string
-}
--- AtencionMedicaRemota/src/app/modelos/historial.ts
+++
@@ -1,7 +0,0 @@
-export class Historial {
-    de_usuario: string;
-    para_usuario: string;
-    mensaje: string;
-    fecha: string;
-
-  }
